1bbc614f5ea6d612e1821c0dca3279e29adba76d
Andy De Ruyter <deruyterandy@gmail.com>
2022-09-04 00:33:55 +0200

    Update inference_codeformer.py

--- inference_codeformer.py
+++ inference_codeformer.py
@@ -12,6 +12,10 @@ import torch.nn.functional as F
 
 from basicsr.utils.registry import ARCH_REGISTRY
 
+torch.cuda.empty_cache()
+os.environ["PYTORCH_CUDA_ALLOC_CONF"]="max_split_size_mb:64"
+
+
 pretrain_model_url = {
     'restoration': 'https://github.com/andyderuyter/CodeFormer/releases/download/v0.1.0/codeformer.pth',
 }
@@ -107,110 +111,155 @@ if __name__ == '__main__':
     else:
         bg_upsampler = None
 
-    # ------------------ set up CodeFormer restorer -------------------
-    net = ARCH_REGISTRY.get('CodeFormer')(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9, 
-                                            connect_list=['32', '64', '128', '256']).to(device)
-    
-    # ckpt_path = 'weights/CodeFormer/codeformer.pth'
-    ckpt_path = load_file_from_url(url=pretrain_model_url['restoration'], 
-                                    model_dir='weights/CodeFormer', progress=True, file_name=None)
-    checkpoint = torch.load(ckpt_path)['params_ema']
-    net.load_state_dict(checkpoint)
-    net.eval()
 
-    # ------------------ set up FaceRestoreHelper -------------------
-    # large det_model: 'YOLOv5l', 'retinaface_resnet50'
-    # small det_model: 'YOLOv5n', 'retinaface_mobile0.25'
-    if not args.has_aligned: 
-        print(f'Using [{args.detection_model}] for face detection network.')
-    face_helper = FaceRestoreHelper(
-        args.upscale,
-        face_size=512,
-        crop_ratio=(1, 1),
-        det_model = args.detection_model,
-        save_ext='png',
-        use_parse=True,
-        device=device)
+    if args.facefix == "Yes":
 
-    # -------------------- start to processing ---------------------
-    # scan all the jpg and png images
-    for img_path in sorted(glob.glob(os.path.join(args.test_path, '*.[jp][pn]g'))):
-        # clean all the intermediate results to process the next image
-        face_helper.clean_all()
+        # ------------------ set up CodeFormer restorer -------------------
+        net = ARCH_REGISTRY.get('CodeFormer')(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9, 
+                                                connect_list=['32', '64', '128', '256']).to(device)
         
-        img_name = os.path.basename(img_path)
-        print(f'Processing: {img_name}')
-        basename, ext = os.path.splitext(img_name)
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        # ckpt_path = 'weights/CodeFormer/codeformer.pth'
+        ckpt_path = load_file_from_url(url=pretrain_model_url['restoration'], 
+                                        model_dir='weights/CodeFormer', progress=True, file_name=None)
+        checkpoint = torch.load(ckpt_path)['params_ema']
+        net.load_state_dict(checkpoint)
+        net.eval()
 
-        if args.has_aligned: 
-            # the input faces are already cropped and aligned
-            img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_LINEAR)
-            face_helper.cropped_faces = [img]
-        else:
-            face_helper.read_image(img)
-            # get face landmarks for each face
-            num_det_faces = face_helper.get_face_landmarks_5(
-                only_center_face=args.only_center_face, resize=640, eye_dist_threshold=5)
-            print(f'\tdetect {num_det_faces} faces')
-            # align and warp each face
-            face_helper.align_warp_face()
+        # ------------------ set up FaceRestoreHelper -------------------
+        # large det_model: 'YOLOv5l', 'retinaface_resnet50'
+        # small det_model: 'YOLOv5n', 'retinaface_mobile0.25'
+        if not args.has_aligned: 
+            print(f'Using [{args.detection_model}] for face detection network.')
+        face_helper = FaceRestoreHelper(
+            args.upscale,
+            face_size=512,
+            crop_ratio=(1, 1),
+            det_model = args.detection_model,
+            save_ext='png',
+            use_parse=True,
+            device=device)
 
-        # face restoration for each cropped face
-        for idx, cropped_face in enumerate(face_helper.cropped_faces):
-            # prepare data
-            cropped_face_t = img2tensor(cropped_face / 255., bgr2rgb=True, float32=True)
-            normalize(cropped_face_t, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
-            cropped_face_t = cropped_face_t.unsqueeze(0).to(device)
+        # -------------------- start to processing ---------------------
+        # scan all the jpg and png images
+        for img_path in sorted(glob.glob(os.path.join(args.test_path, '*.[jp][pn]g'))):
+            # clean all the intermediate results to process the next image
+            face_helper.clean_all()
+            
+            img_name = os.path.basename(img_path)
+            print(f'Processing: {img_name}')
+            basename, ext = os.path.splitext(img_name)
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
-            try:
-                with torch.no_grad():
-                    output = net(cropped_face_t, w=w, adain=True)[0]
-                    restored_face = tensor2img(output, rgb2bgr=True, min_max=(-1, 1))
-                del output
-                torch.cuda.empty_cache()
-            except Exception as error:
-                print(f'\tFailed inference for CodeFormer: {error}')
-                restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
-
-            restored_face = restored_face.astype('uint8')
-            face_helper.add_restored_face(restored_face)
-
-        # paste_back
-        if not args.has_aligned:
-            # upsample the background
-            if bg_upsampler is not None:
-                # Now only support RealESRGAN for upsampling background
-                bg_img = bg_upsampler.enhance(img, outscale=args.upscale)[0]
+            if args.has_aligned: 
+                # the input faces are already cropped and aligned
+                img = cv2.resize(img, (512, 512), interpolation=cv2.INTER_LINEAR)
+                face_helper.cropped_faces = [img]
             else:
-                bg_img = None
-            face_helper.get_inverse_affine(None)
-            # paste each restored face to the input image
+                face_helper.read_image(img)
+                # get face landmarks for each face
+                num_det_faces = face_helper.get_face_landmarks_5(
+                    only_center_face=args.only_center_face, resize=640, eye_dist_threshold=5)
+                print(f'\tdetect {num_det_faces} faces')
+                # align and warp each face
+                face_helper.align_warp_face()
+
+            # face restoration for each cropped face
+            for idx, cropped_face in enumerate(face_helper.cropped_faces):
+                # prepare data
+                cropped_face_t = img2tensor(cropped_face / 255., bgr2rgb=True, float32=True)
+                normalize(cropped_face_t, (0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True)
+                cropped_face_t = cropped_face_t.unsqueeze(0).to(device)
+
+                try:
+                    with torch.no_grad():
+                        output = net(cropped_face_t, w=w, adain=True)[0]
+                        restored_face = tensor2img(output, rgb2bgr=True, min_max=(-1, 1))
+                    del output
+                    torch.cuda.empty_cache()
+                except Exception as error:
+                    print(f'\tFailed inference for CodeFormer: {error}')
+                    restored_face = tensor2img(cropped_face_t, rgb2bgr=True, min_max=(-1, 1))
+
+                restored_face = restored_face.astype('uint8')
+                face_helper.add_restored_face(restored_face)
+
+            # paste_back
+            if not args.has_aligned:
+                # upsample the background
+                if bg_upsampler is not None:
+                    # Now only support RealESRGAN for upsampling background
+                    bg_img = bg_upsampler.enhance(img, outscale=args.upscale)[0]
+                else:
+                    bg_img = None
+                face_helper.get_inverse_affine(None)
+                # paste each restored face to the input image
+                restored_img = face_helper.paste_faces_to_input_image(upsample_img=bg_img, draw_box=args.draw_box)
+
+            # save faces
+            for idx, (cropped_face, restored_face) in enumerate(zip(face_helper.cropped_faces, face_helper.restored_faces)):
+                # save cropped face
+                if not args.has_aligned: 
+                    save_crop_path = os.path.join(result_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
+                    imwrite(cropped_face, save_crop_path)
+                # save restored face
+                if args.has_aligned:
+                    save_face_name = f'{basename}.png'
+                else:
+                    save_face_name = f'{basename}_{idx:02d}.png'
+                save_restore_path = os.path.join(result_root, 'restored_faces', save_face_name)
+                imwrite(restored_face, save_restore_path)
+
+            # save restored img
+            if not args.has_aligned and restored_img is not None:
+                if args.facefix == "Yes":
+                    save_restore_path = os.path.join(result_root, 'final_results', f'{basename}_{args.bg_upsampler}_{args.w}.png')
+                    imwrite(restored_img, save_restore_path)
+                    print('Face restored.')
+
+        print(f'\nAll results are saved in {result_root}')
+
+    if args.facefix == "No":
+
+        # ------------------ set up CodeFormer restorer -------------------
+        net = ARCH_REGISTRY.get('CodeFormer')(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9, 
+                                                connect_list=['32', '64', '128', '256']).to(device)
+        
+        # ckpt_path = 'weights/CodeFormer/codeformer.pth'
+        ckpt_path = load_file_from_url(url=pretrain_model_url['restoration'], 
+                                        model_dir='weights/CodeFormer', progress=True, file_name=None)
+        checkpoint = torch.load(ckpt_path)['params_ema']
+        net.load_state_dict(checkpoint)
+        net.eval()
+
+        # ------------------ set up FaceRestoreHelper -------------------
+        # large det_model: 'YOLOv5l', 'retinaface_resnet50'
+        # small det_model: 'YOLOv5n', 'retinaface_mobile0.25'
+        if not args.has_aligned: 
+            print(f'Using [{args.detection_model}] for face detection network.')
+        face_helper = FaceRestoreHelper(
+            args.upscale,
+            face_size=512,
+            crop_ratio=(1, 1),
+            det_model = args.detection_model,
+            save_ext='png',
+            use_parse=True,
+            device=device)
+
+        # -------------------- start to processing ---------------------
+        # scan all the jpg and png images
+        for img_path in sorted(glob.glob(os.path.join(args.test_path, '*.[jp][pn]g'))):
+            # clean all the intermediate results to process the next image
+            face_helper.clean_all()
+            
+            img_name = os.path.basename(img_path)
+            print(f'Processing: {img_name}')
+            basename, ext = os.path.splitext(img_name)
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+            bg_img = bg_upsampler.enhance(img, outscale=args.upscale)[0]
             restored_img = face_helper.paste_faces_to_input_image(upsample_img=bg_img, draw_box=args.draw_box)
+            save_restore_path = os.path.join(result_root, 'final_results', f'{basename}_{args.bg_upsampler}_noface.png')
+            imwrite(bg_img, save_restore_path)
+            print('No face restored.')
 
-        # save faces
-        for idx, (cropped_face, restored_face) in enumerate(zip(face_helper.cropped_faces, face_helper.restored_faces)):
-            # save cropped face
-            if not args.has_aligned: 
-                save_crop_path = os.path.join(result_root, 'cropped_faces', f'{basename}_{idx:02d}.png')
-                imwrite(cropped_face, save_crop_path)
-            # save restored face
-            if args.has_aligned:
-                save_face_name = f'{basename}.png'
-            else:
-                save_face_name = f'{basename}_{idx:02d}.png'
-            save_restore_path = os.path.join(result_root, 'restored_faces', save_face_name)
-            imwrite(restored_face, save_restore_path)
-
-        # save restored img
-        if not args.has_aligned and restored_img is not None:
-            if args.facefix == "Yes":
-                save_restore_path = os.path.join(result_root, 'final_results', f'{basename}_{args.bg_upsampler}_{args.w}.png')
-                imwrite(restored_img, save_restore_path)
-                print('Face restored.')
-            if args.facefix == "No":
-                save_restore_path = os.path.join(result_root, 'final_results', f'{basename}_{args.bg_upsampler}_noface.png')
-                imwrite(bg_img, save_restore_path)
-                print('No face restored.')
-
-    print(f'\nAll results are saved in {result_root}')
+        print(f'\nAll results are saved in {result_root}')
